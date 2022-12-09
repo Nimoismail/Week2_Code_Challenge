d0418d74a331727beb5d068a1bbad7293e85bb91
nimo <nimo.sheikh@student.moringaschool.com>
2022-12-09 12:11:00 +0300

    compleleted index.js

--- index.js
+++ index.js
@@ -1,49 +1,39 @@
-const div = document.getElementById("character-bar");
-const url = "http://localhost:3000/characters"  //this is the API which is hosted inside localhost dbjson
-
-
-// this function fetches data from server
-function getAllCharacters() {
-  fetch(url)    //API
-    .then((res) => res.json()) //the response brings a promise then converted in json format
-    .then((data) => {
-      //we are looping through the array data of each character
-      data.forEach((animals) => {
-        let span = document.createElement("span");
-        span.textContent = animals.name;
-        span.addEventListener("click", (e) => {
-          let pname = document.getElementById("name");
-          pname.textContent = animals.name;
-          let img = document.getElementById("image");
-          img.src = animals.image;
-          let voteCounts = document.getElementById("vote-count");
-          voteCounts.textContent = animals.votes;
-        });
-        div.appendChild(span);
-      });
-    });
-}
-getAllCharacters();
-
-
-// form submit
-let form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  // counts votes and returns when clicked
-  let countVote = document.getElementById("votes").value;
-  console.log(countVote);
-  let totalVotes = document.getElementById("vote-count");
-  // add votes to the previous votes on each character
-  let total = parseInt(totalVotes.innerHTML) + parseInt(countVote, 10);
-  totalVotes.innerHTML = total;
-  form.reset();
-});
-
-
-//resets the count to 0
-let reset = document.getElementById("reset-btn");
-reset.addEventListener("click", (e) => {
-  let totalVotes = document.getElementById("vote-count");
-  totalVotes.innerHTML = 0;
-});
+// loads the dom before any other functionality
+document.addEventListener("DOMContentLoaded", () => {
+  getCats()
+  })
+  //  endpoint to fetch data
+  const url= "http://localhost:3000/characters"
+  // this function fetchs data and converts it to the json
+              function getCats(){
+                  fetch(url)
+                  .then(response => response.json())
+                  .then(cats => {
+                      cats.forEach(cat => {
+                          displayCats(cat)
+                      });
+                  })
+              }
+  //this function displays Week2-code-challenge animals
+              function displayCats(cat){
+                  const name= document.createElement('p')
+                  name.innerHTML=cat.name
+                  const main= document.getElementById("main")
+                  main.append(name)
+                  name.addEventListener("click", () => {
+                      singleCat(cat)
+                  })
+              }
+          //  handles all Week2-code-challenge animals characteristics
+  function singleCat(cat){
+      document.getElementById("catName").innerHTML=cat.name
+      document.getElementById("catImg").src = cat.image
+      const btn = document.getElementById("catLikes")
+          btn.textContent= `Likes ${cat.votes}`
+          // the event listener adds votes to the clicked Week2-code-challenge
+      btn.addEventListener("click", ()=> {
+         cat.votes++
+         btn.textContent= `Likes ${cat.votes}`
+      })
+  }
+      btn.innerHTML=`Likes: ${Likes}`
